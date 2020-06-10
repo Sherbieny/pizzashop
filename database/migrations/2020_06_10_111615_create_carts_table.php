13cf489231a6bc9581522060da3f95638bc8fe62
Eslam El-Sherbieny <sherbieny@outlook.com>
2020-06-10 14:42:55 +0300

    cart model created

---
+++ database/migrations/2020_06_10_111615_create_carts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('qty')->nullable();
+            $table->decimal('total', 8, 3)->nullable();
+            $table->string('address')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->bigInteger('customer_id')->nullable()->unsigned();
+            $table->foreign('customer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('customer_email')->nullable();
+            $table->string('customer_firstname')->nullable();
+            $table->string('customer_lastname')->nullable();
+            $table->string('customer_firstname')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
